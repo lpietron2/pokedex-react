d6fb8bb5f47b8a0eabba33462fca2e1ba5c4e89c
unknown <lpietron@wszib.edu.pl>
2021-11-25 12:12:40 +0100

    added date for no reason

--- src/App.js
+++ src/App.js
@@ -21,6 +21,25 @@ function App() {
     }
   }
 
+  const dateBuider = (d) =>{
+    let day = d.getDate();
+    let month = d.getMonth();
+    let year = d.getFullYear();
+
+    let hour = d.getHours();
+    let minutes = d.getMinutes();
+
+    if(hour < 10){
+      hour = "0"+ hour;
+    }
+    if(minutes < 10){
+      minutes = "0"+ minutes;
+    }
+    
+
+    return hour + ":" + minutes + " " + day + "." + month + "." + year;
+  }
+
   return (
     <div className="App">
       <img className="logo" src={logo} alt='Pokedex' />
@@ -35,6 +54,7 @@ function App() {
         />
       </div>
       <ResultControler pokemon={pokemon} isShiny={isShiny} setIsShiny={setIsShiny}/>
+      <div className="date-box">{dateBuider(new Date())}</div>
     </div>
   );
 }
